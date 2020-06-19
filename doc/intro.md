e0ce930825fa049f69128fa422280e340e902894
Mitchel Kuijpers <mitchelkuijpers@gmail.com>
2020-06-19 12:36:25 +0200

    Initial project scaffold

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to datastore
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/avisi/crux/datastore_test.clj
@@ -0,0 +1,7 @@
+(ns avisi.crux.datastore-test
+  (:require [clojure.test :refer :all]
+            [avisi.crux.datastore :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
